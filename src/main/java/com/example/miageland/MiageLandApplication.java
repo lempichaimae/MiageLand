221f5d98bae33bef095618dbc3fcd72d43e156a8
lempichaimae <chaimae.khamma@gmail.com>
2023-05-24 16:09:21 +0200

    initial commit

---
+++ src/main/java/com/example/miageland/MiageLandApplication.java
@@ -0,0 +1,13 @@
+package com.example.miageland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiageLandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiageLandApplication.class, args);
+    }
+
+}
